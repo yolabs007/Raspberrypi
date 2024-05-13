ee2668bcdabdc7ae5c74e54ad8eb22108d6f5a06
YoLabs <59382666+yolabs007@users.noreply.github.com>
2024-05-13 09:32:37 +0530

    Create forloop.py

---
+++ simple scripts/forloop.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.written by rahul sharma
+# written for Yolabs classes for PY&Pi
+# 13/05/2024
+# this code will help you to learn basic of the for loop
+# code to increate the numbers:
+
+for i in range(10):
+    print("first loop", i)
+
+for i in range(2,10):
+    print("second loop",i)
+
+for i in range(1,10,3):
+    print("third loop", i)
+
+for i in range(10,1,-1):
+    print("fourth loop", i)
