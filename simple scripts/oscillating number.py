7265897b4cf5f21c6ae6859ec49f93dd281876c3
YoLabs <59382666+yolabs007@users.noreply.github.com>
2024-05-13 09:56:36 +0530

    Create oscillating number.py

---
+++ simple scripts/oscillating number.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.written by rahul sharma
+# written for Yolabs classes for PY&Pi
+# 13/05/2024
+# this code will help you to learn basic of the for loop and hw to oscillate a number
+# code to increate the numbers:
+
+import time
+
+while True:
+    for i in range(1,10):
+        print(i)
+        time.sleep(0.3)
+    for i in range(10,1,-1):
+        print(i)
+        time.sleep(0.3)
