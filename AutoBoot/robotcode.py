9a606b271758c44a51748daa371708853e52795e
YoLabs <59382666+yolabs007@users.noreply.github.com>
2025-01-03 21:28:55 +0530

    Update robotcode.py

--- AutoBoot/robotcode.py
+++ AutoBoot/robotcode.py
@@ -1,4 +1,4 @@
-""" Your First Raspberry PI project 
+""" we are using Blink LED code for checking that robotcode will work automatically or not 
 Lets make one LED Blink 
 Note: the pin number you selelcted varies based on 
 
@@ -17,10 +17,10 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering, please refer the pin diagram 
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 
 while True: # Run forever
-  GPIO.output(8, GPIO.HIGH) # Turn on
+  GPIO.output(7, GPIO.HIGH) # Turn on
   sleep(1) # Sleep for 1 second
-  GPIO.output(8, GPIO.LOW) # Turn off
+  GPIO.output(7, GPIO.LOW) # Turn off
   sleep(1) # Sleep for 1 second
